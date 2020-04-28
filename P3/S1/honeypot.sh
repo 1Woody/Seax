0e5624dbebb99b751806891513562874c521d367
1Woody <daniel.benavente4@gmail.com>
2020-04-28 19:37:15 +0200

    Co-authored-by: CesarTongoliki <infocesarguti@gmail.com>

--- P3/S1/honeypot.sh
+++ P3/S1/honeypot.sh
@@ -1,55 +1,68 @@
 #!/bin/bash
-
+# Elegir bien los PERMISOS!!!
 # ports --> https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
 
-invalidarguments='El nombre de arguments és incorrecte. Han de ser 3 arguments (revisar manual de usuari)'
-interfaces= basename -a /sys/class/net/*
-usage='Usage: honeypot.sh'
+usageInvalidArg="El nombre de arguments és incorrecte. Han de ser 3 arguments (revisar manual de usuari)."
+usagePortEnter="Recorda, l'argument del port ha de contenir un nombre enter."
+usagePortRang="Compte! El port s'ha de trobar entre el 0 i el 65535 (ports disponibles)."
+usageProtocolInc="El protocol escollit no està permés. Recorda que ha de ser TCP, UDP o ICMP."
+usageInterficieInc="La interfície no és vàlida o no es troba al sistema."
+llistaInterficies=$(ls /sys/class/net/)
+i=0
+PR="$2"
+protocolMajus=${PR^^}
+protocolMinus=${PR,,}
+primeraHora="primeraHora"
+ultimaHora="ultimaHora"
+myIP=$(hostname -I)
 
 #detecció de la correctesa dels arguments d'entrada --> ./honeypot.sh eth0 tcp 80
 if [ $# == 3 ]
 then
     # Comprovació del port
     if [[ ! $3 =~ ^[0-9]+$ ]] 
-    then
-        echo "Recorda, l'argument del port ha de contenir un nombre enter."; 
-        echo $usage1; exit 1
+    then 
+        echo "$usagePortEnter"; 
+        exit 1
     elif [ "$3" -gt "65535" ] || [ "$3" -lt "0" ]
     then
-        echo "Compte! El port s'ha de trobar entre el 0 i el 65535 (ports disponibles).";
-        echo $usage; exit 1    
+        echo "$usagePortRang"; exit 1    
     # Comprovació del protocol
-    elif [ "$2" != "TCP" ] && [ "$2" !="UDP" ] && [ "$2" != "ICMP" ]
+    elif [ "$protocolMajus" != "TCP" ] && [ "$protocolMajus" != "UDP" ] && [ "$protocolMajus" != "ICMP" ]
     then
-        echo "El protocol escollit no està permés. Recorda que ha de ser TCP, UDP o ICMP." 
-        echo $usage; exit 1
+        echo "$usageProtocolInc"; exit 1
     else
-        i=0
-        for interface in $(ls /sys/class/net/); do # basename -a /sys/class/net/*
+        for interface in $llistaInterficies; do
             if [ "$interface" == "$1" ]
             then
-                ((i=i+1))
-                echo $i
+                ((i+=1))
             fi
         done
-        if [ cont != 1 ]
+        if [ "$i" != 1 ]
         then 
-            echo "La interfície no és vàlida o no es troba al sistema."
-            echo $usage; exit 1
+            echo "$usageInterficieInc"; exit 1
         fi
-        #
-        #
-        #
-        #
-        #
-        # CÓDIGO DEL SCRIPT (SIN ERRORES DE ARGUMENTOS)
-        #
-        #
-        #
-        #
-        #
+        echo -e ""\\n
+        echo -e "-----------------------------------------------------------------------------"
+        echo -e "Accesos a l'adreça $myIP port $protocolMinus $3 [$primeraHora , $ultimaHora] "
+        echo -e "-----------------------------------------------------------------------------"\\n
+        echo -e "------------------------------"
+        echo -e "Resum dels accessos"
+        echo -e "------------------------------"
+        echo -e "    Adreces IP     Nº accessos"
+        echo -e " ---------------   -----------"
+        #CÓDIGO RESUMEN DE LOS ACCESOS
+        echo -e " ---------------   -----------"\\n
+        echo -e "--------------------------------------"
+        echo -e "Evolució dels accessos"
+        echo -e "--------------------------------------"
+        echo -e "      Temps         Adreça IP     Port"
+        echo -e " --------------- --------------- -----"
+        #CÓDIGO EVOLUCIÓN DE LOS ACCESOS
+        echo -e " --------------- --------------- -----"\\n
+        echo -e "Prem [q] per sortir."\\n
+        
     fi
 else
-    echo $invalidarguments; 
-    echo $usage; exit 1
+    echo "$usageInvalidArg"; exit 1
 fi
