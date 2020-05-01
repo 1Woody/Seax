40e914e8dffb53c02f2417c211d9b8facf824884
1Woody <daniel.benavente4@gmail.com>
2020-05-01 21:24:06 +0200

    Versión funcional: Falta formato + verificar (pkg)

--- P3/S1/honeypot_1sim.sh
+++ P3/S1/honeypot_1sim.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#!utf-8
 # Elegir bien los PERMISOS!!!
-# COMPROBAR LO QUE NECESITA EL USUARIO (PAQUETES, ROOT ...)
 # ports --> https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
 
 # Variables de usage per els avisos de error
@@ -35,27 +33,15 @@ filter_icmp="'not src "$myIP" && icmp'"
 filter_tcp="tcp[13]=2 && port "$3""
 
 #Variables de loop
-numAccesosSimultanis=11 #10 accesos, agafem 11 per evitar agafar la capçalera
-comptLinia=2
-arrayAtacs+=("") # array amb comptador de repetició
-arrayFullAtacs+=("") # array amb tots els atacs (sense tenir en compte repeticions)
+arrayAtacs+=("")
 repetit=0
-primeraHora=" 0 atacs rebuts"
-ultimaHora=" 0 atacs rebuts"
+primeraHora="primeraHora"
+ultimaHora="ultimaHora"
+arrayResum=("      10.1.1.105             2" "      10.1.1.108             2" "      10.1.1.105             2" "      10.1.1.105             2")
+arrayProva=("18:30:12.5432-192.168.0.27-22-1" "12:31:12.5531-192.168.0.28-50000-2" "08:59:00.5432-192.168.0.33-123-1" "18:30:12.5432-192.168.0.27-6543-1" "18:30:12.5432-192.168.0.27-88-4")
+#arrayEvol=(" 17:33:30.806049      10.1.1.108 51006" " 17:33:41.596338      10.1.1.105 40690" " 17:33:43.926395      10.1.1.105 40692" " 17:33:50.541803      10.1.1.108 51008")
 
 ####### PROGRAMA #######
-
-#detecció dels progrmas necessaris
-if [ $(whoami) != "root" ]; then #has de ser root (whoami et diu si ho executes com root)
-	echo "Has de ser root per executar aquest script"
-	exit 1
-fi
-
-if [ $(man tcpdump 2>&1 | wc -l) -eq 1 ]; then #Si busquem el manual de tcpdump i no trobem res significa que no tenim instalat aquesta comanda
-	echo "Has de tenir instalat el paquet de tcpdump, instala amb: apt install tcpdump"
-	exit 1
-fi
-
 
 #detecció de la correctesa dels arguments d'entrada
 if [ $# == 2 ]
@@ -101,6 +87,8 @@ fi
 # Creació de fitxers necessaris
 touch log_honeypot
 touch atacs.log
+touch test.log
+true >  test.log
 
 # Activació del procés de tcpdump en funció del paràmetres establerts
 echo -e "ÚLTIM ACCÈS REGISTRAT" > atacs.log
@@ -124,51 +112,48 @@ horaCompilacioInici=$(date | cut -d ' ' -f5)
 tput sc; 
 while [ $quit != 1 ]
 do
-    while [ "$comptLinia" -le "$numAccesosSimultanis" ]
-    do
-        hora=$(awk -v line=$comptLinia '/./{if(NR==line) print $1}' atacs.log)
-        ipNouAtac=$(awk -v line=$comptLinia '/./{if(NR==line) print $3}' atacs.log | cut -d '.' -f1,2,3,4)
-        port=$(awk -v line=$comptLinia '/./{if(NR==line) print $3}' atacs.log | cut -d '.' -f5)
-        if [ "$hora" != "" ] && [ "$ipNouAtac" != "" ]
-        then 
-            atacActual="$hora-$ipNouAtac-$port"
-            if [ "${arrayAtacs[0]}" == "" ] && [ "${arrayFullAtacs[0]}" == "" ]
-            then
-                primeraHora=$hora #guardem l'hora del primer acces
-                arrayFullAtacs[0]="$atacActual"
-                atacActual="$atacActual-1"
-                arrayAtacs[0]="$atacActual"
-            else
-                ultimaHora=$hora #guardem l'hora de l'últim accés
-                arrayFullAtacs+=($atacActual)
-                for pos in "${!arrayAtacs[@]}"
-                do  
-                    ipAtac=$(echo "${arrayAtacs[$pos]}" | cut -d '-' -f2)
-                    if [ "$ipNouAtac" == "$ipAtac" ]
-                    then
-                        auxValors=$(echo "${arrayAtacs[$pos]}" | cut -d '-' -f1,2,3)
-                        comptAtacs=$(echo "${arrayAtacs[$pos]}" | cut -d '-' -f4 | awk '{print $0+1}')
-                        nouAtac="$auxValors-$comptAtacs"
-                        arrayAtacs[$pos]="$nouAtac"
-                        repetit=1
-                        break
-                    fi
-                done
-                if [ "$repetit" != 1 ]
-                then
-                    atacActual="$atacActual-1"
-                    arrayAtacs+=($atacActual)
-                fi
-                repetit=0
-            fi
-            ((comptLinia+=1))
+    
+    hora=$(awk '/./{if(NR==2) print $1}' atacs.log)
+    ipNouAtac=$(awk '/./{if(NR==2) print $3}' atacs.log | cut -d '.' -f1,2,3,4)
+    port=$(awk '/./{if(NR==2) print $3}' atacs.log | cut -d '.' -f5)
+    if [ "$hora" != "" ] && [ "$ipNouAtac" != "" ]
+    then 
+        atacActual="$hora-$ipNouAtac-$port"
+        if [ "${arrayAtacs[0]}" == "" ]
+        then
+            atacActual="$atacActual-1"
+            arrayAtacs[0]="$atacActual"
         else
-            comptLinia=12  # acabem la lectura ja que no hi ha res al fitxer
+            for pos in "${!arrayAtacs[@]}"
+            do  
+                ipAtac=$(echo "${arrayAtacs[$pos]}" | cut -d '-' -f2)
+                echo "$ipNouAtac" >> test.log
+                echo "$ipAtac" >> test.log
+                if [ "$ipNouAtac" == "$ipAtac" ]
+                then
+                    auxValors=$(echo "${arrayAtacs[$pos]}" | cut -d '-' -f1,2,3)
+                    comptAtacs=$(echo "${arrayAtacs[$pos]}" | cut -d '-' -f4 | awk '{print $0+1}')
+                    nouAtac="$auxValors-$comptAtacs"
+                    arrayAtacs[$pos]="$nouAtac"
+                    repetit=1
+                    break
+                fi
+            done
+            echo $repetit
+            if [ "$repetit" != 1 ]
+            then
+                atacActual="$atacActual-1"
+                arrayAtacs+=($atacActual)
+            fi
+            repetit=0
         fi
-    done
-    comptLinia=2
-    echo -e "ÚLTIM ACCÈS REGISTRAT" > atacs.log 
-
+        echo -e "ÚLTIM ACCÈS REGISTRAT" > atacs.log 
+        echo ------ARRAY------- >> test.log
+        for i in "${!arrayAtacs[@]}"
+        do
+            printf 'arrayAtacs[%s]=%s\n' "$i" "${arrayAtacs[$i]}" >> test.log
+        done
+    fi
     ####################INICI FORMAT####################
     tput ed;
     read -r -t 0.01 -N 1 input
@@ -212,12 +197,12 @@ do
     echo -e "--------------------------------------"
     echo -e "      Temps         Adreça IP     Port"
     echo -e " --------------- --------------- -----"
-    for each in "${!arrayFullAtacs[@]}"
+    for each in "${!arrayAtacs[@]}"
     do
-        horaAcces=$( echo "${arrayFullAtacs[$each]}" |cut -d '-' -f1 )
-        ipAcces=$( echo "${arrayFullAtacs[$each]}" |cut -d '-' -f2 )
-        portAcces=$( echo "${arrayFullAtacs[$each]}" |cut -d '-' -f3 )
-        stringResum=" $horaAcces   $ipAcces   $portAcces"
+        horaAcces=$( echo "${arrayAtacs[$each]}" |cut -d '-' -f1 )
+        ipAcces=$( echo "${arrayAtacs[$each]}" |cut -d '-' -f2 )
+        portAcces=$( echo "${arrayAtacs[$each]}" |cut -d '-' -f3 )
+        stringResum="$ $horaAcces   $ipAcces   $portAcces"
         echo "$stringResum"
     done
     echo -e " --------------- --------------- -----"
